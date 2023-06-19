3990d1973809050b3ed8130da21c391b75a01f5d
HCE <xhce911@gmail.com>
2023-06-19 01:24:01 -0500

    removing form

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -23,7 +23,7 @@ export default class gallery extends Component {
       <>
        <section className="hero ptrn is-fluid is-fullheight is-vcentered">
             <div className="has-text-centered">
-              <h1 className="pacific is-size-1 gname ">Chaak Baltazar</h1>
+              <h1 className="pacific is-size-1 gname ">Chaak Balthazar</h1>
               <h4 className="pacific is-size-4 gname">Campanur Ramírez</h4>
             <Swiper
                 effect={"cards"}
