c42eba99e6d0fb84c78012bd43cd0045eb78eada
HCE <xhce911@gmail.com>
2023-10-27 17:19:41 -0600

    changes5.3

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ export default class footer extends Component {
   render() {
     return (
       <>
-      <div className='deco1'></div>
+      
          <footer class="footer">
             <div class="content has-text-centered">
                 <p>
@@ -12,6 +12,7 @@ export default class footer extends Component {
                 </p>
             </div>
         </footer>
+        <div className='deco1'></div>
       </>
     )
   }
